f6fd26f62983a8670caebb556187c59e174d65fd
lewah <ingrid.lewah@gmail.com>
2022-07-06 22:11:00 +0300

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-function that reads a text file
-"""
+#function that reads a text file
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout
     Returns none
